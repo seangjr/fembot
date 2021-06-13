5193d86d0b3e181d5422cddb1929852b53d93ddb
sean <tp063366@mail.apu.edu.my>
2021-06-13 17:52:54 +0800

    edited bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
 var Chance = require('chance');
 const client = new Discord.Client();
 
@@ -101,4 +100,4 @@ client.on("message", function (message) {
 
 });
 
-client.login(config.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
