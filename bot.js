0ce6be513dec406a482b62e0472171ceb5c683e1
sean <tp063366@mail.apu.edu.my>
2021-06-13 20:18:46 +0800

    replaced some roles and added new ones

--- bot.js
+++ bot.js
@@ -20,10 +20,11 @@ client.on("message", function (message) {
   // roles
   let ganyuGrace = message.guild.roles.cache.find(r => r.id === "853529776307961856");
   let lovedByGod = message.guild.roles.cache.find(r => r.id === "853539423542968350");
-  let bakaMitai = message.guild.roles.cache.find(r => r.id === "853543265486897162");
+  let gingerFav = message.guild.roles.cache.find(r => r.id === "853543265486897162");
   let simpRole = message.guild.roles.cache.find(r => r.id === "842010444478939146");
   let shame = message.guild.roles.cache.find(r => r.id === "841893441059553320");
   let rickrole = message.guild.roles.cache.find(r => r.id === "853588881441423371");
+  let gachaAddict = message.guild.roles.cache.find(r => r.id === "853608039214612511");
 
 
   //member rolling function
@@ -49,21 +50,27 @@ client.on("message", function (message) {
 
       var result = roll();
 
+      //ganyu's grace
       if(result == 1 ) {
         member.roles.add(ganyuGrace);
           return `congrats <@${message.author.id}>! you just got the rarest <@&853529776307961856>!`;
       }
-
+      //loved by GOD
       if(result >= 2 && result <= 4) {
         member.roles.add(lovedByGod);
           return `nice <@${message.author.id}> you're loved by GOD!`;
       }
-
-      if(result >= 5 && result <= 10) {
-        member.roles.add(bakaMitai);
-          return 'you got the <@&853543265486897162> role!';
+      //gacha addict
+      if (result >= 5 && result <= 7) {
+        member.roles.add(gachaAddict);
+          return 'why are you so addicted to this?! <@&853608039214612511> given. noice.';
       }
-
+      //ginger fav
+      if(result >= 8 && result <= 10) {
+        member.roles.add(gingerFav);
+          return "you're officially <@306794812282109953>'s favourite! :sparkles: :sparkles:";
+      }
+      //simp
       if(result >= 10 && result <= 15) {
         member.roles.add(simpRole);
         setTimeout(() => {
@@ -71,12 +78,12 @@ client.on("message", function (message) {
         }, 1.728E+8);
           return 'lol simp! you got the simp role lmao haha';
       }
-
+      
       if(result >= 16 && result <= 20) {
         return 'you almost got lucky <3 sadge';
       }
 
-      
+      //rick role
       if(result >= 21 && result <= 40) {
         member.roles.add(rickrole);
         setTimeout(() => {
@@ -86,7 +93,7 @@ client.on("message", function (message) {
           return `No reward!`;
       }
             
-
+      //mute
       if(result >= 41 && result <= 70) {
         member.roles.add(shame);
         setTimeout(() => {
@@ -95,7 +102,7 @@ client.on("message", function (message) {
           return "lmao i can't stop laughing u got muted for 20mins";
       }
       
-
+      
       if(result >= 71 && result <=100) {
           return 'I had your reward but <@393373395149914113> stole it';
       }
