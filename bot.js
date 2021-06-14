857c9f5384fc2c94c6ee0bdebb44266127771be0
sean <tp063366@mail.apu.edu.my>
2021-06-14 10:26:35 +0800

    updated to async

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('ready', () => {
 });
 //chance
 var chance = new Chance();
-var roll = function() {
+async var roll = function() {
     return chance.integer({ min: 1, max: 200 });
 }
 
@@ -27,7 +27,7 @@ let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d);
 
 const prefix = ">";
 
-client.on("message", function (message) {
+client.on("message", async function (message) {
 
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
@@ -84,67 +84,67 @@ client.on("message", function (message) {
    * gacha roles function
    * - has chance function
    */
-  var pull = function() {
+  async var pull = function() {
 
       var result = roll();
 
       //ganyu's grace
-      if(result == 1 ) {
-        member.roles.add(ganyuGrace);
-          return `congrats <@${message.author.id}>! you just got the rarest <@&853529776307961856>!`;
+      if(result === 1 ) {
+        await member.roles.add(ganyuGrace);
+          return await `congrats <@${message.author.id}>! you just got the rarest <@&853529776307961856>!`;
       }
       //loved by GOD
-      if(result >= 2 && result <= 4) {
-        member.roles.add(lovedByGod);
-          return `nice <@${message.author.id}> you're loved by GOD!`;
+      else if(result >= 2 && result <= 4) {
+        await member.roles.add(lovedByGod);
+          return await `nice <@${message.author.id}> you're loved by GOD!`;
       }
       //gacha addict
-      if (result >= 5 && result <= 7) {
-        member.roles.add(gachaAddict);
+      else if (result >= 5 && result <= 7) {
+        await member.roles.add(gachaAddict);
           return 'why are you so addicted to this?! <@&853608039214612511> given. noice.';
       }
       //ginger fav
-      if(result >= 8 && result <= 10) {
-        member.roles.add(gingerFav);
+      else if(result >= 8 && result <= 10) {
+        await member.roles.add(gingerFav);
           return "you're officially <@306794812282109953>'s favourite! :sparkles: :sparkles:";
       }
       //simp
-      if(result >= 10 && result <= 15) {
-        member.roles.add(simpRole);
+      else if(result >= 10 && result <= 15) {
+        await member.roles.add(simpRole);
         setTimeout(() => {
-          member.roles.remove(simpRole);
+          await member.roles.remove(simpRole);
         }, 1.728E+8);
           return 'lol simp! you got the simp role lmao haha';
       }
       
-      if(result >= 16 && result <= 20) {
+      else if(result >= 16 && result <= 20) {
         return 'you almost got lucky <3 sadge';
       }
 
       //rick role
-      if(result >= 21 && result <= 40) {
-        member.roles.add(rickrole);
+      else if(result >= 21 && result <= 40) {
+        await member.roles.add(rickrole);
         setTimeout(() => {
-          member.roles.remove(rickrole);
+          await member.roles.remove(rickrole);
         }, 1.728E+8);
-        message.author.send(rickroleMessage);
-          return `No reward!`;
+        await message.author.send(rickroleMessage);
+          return 'No reward!';
       }
             
       //mute
-      if(result >= 41 && result <= 70) {
-        member.roles.add(shame);
+      else if(result >= 41 && result <= 70) {
+        await member.roles.add(shame);
         setTimeout(() => {
-          member.roles.remove(shame);
+          await member.roles.remove(shame);
         }, 1.2E+6);
           return "lmao i can't stop laughing u got muted for 20mins";
       }
       
-      if(result >= 71 && result <=100) {
+      else if(result >= 71 && result <=100) {
           return 'I had your reward but <@393373395149914113> stole it';
       }
 
-      if(result >= 101 && result <= 120) {
+      else if(result >= 101 && result <= 120) {
           return 'No reward!';
       }
       
@@ -164,26 +164,21 @@ client.on("message", function (message) {
     message.channel.send(`Pong! This message had a latency of ${Math.round(client.ws.ping)} ms.`)
   }
 
-  if (command === "gacha") {
-    message.channel.send(gachaEmbed);
+  else if (command === "gacha") {
+    await message.channel.send(gachaEmbed);
   }
 
-  if (command === "help") {
+  else if (command === "help") {
     message.channel.send(help);
   }
 
-  if (member.roles.cache.has('841246634267377675')) {
+  else if (member.roles.cache.has('841246634267377675')) {
     if (command === "changelog") {
       message.channel.send(changelog);
     } 
-
-  } else {
-    if (command === "changelog") {
-      message.channel.send(invalidCmd);
-    }
   }
-
-  if (command === "homework") { 
+  
+  else if (command === "homework") { 
     message.channel.send(homework);
   }
 
